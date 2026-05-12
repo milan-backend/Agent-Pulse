848d03bf3a0e329f81c28af0b6c1f608d5da021a
milan-backend <milancharan847@gmail.com>
2026-05-12 16:36:11 +0530

    fix celery redis

--- app/core/celery_app.py
+++ app/core/celery_app.py
@@ -1,9 +1,12 @@
+import os
 from celery import Celery
+
+REDIS_URL = os.getenv("REDIS_URL")
 
 celery = Celery(
     "dag_worker",
-    broker="redis://redis:6379/0",
-    backend="redis://redis:6379/0"
+    broker=REDIS_URL,
+    backend=REDIS_URL
 )
 
 celery.conf.task_track_started = True
